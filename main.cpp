823d97870894d4b1af9c20c7388de48c3b15f184
catalyst26 <43795576+catalyst26@users.noreply.github.com>
2019-04-21 21:00:51 -0700

    Add files via upload
    program to calculate fibonacci sequence for any number of terms you want

--- main.cpp
+++ main.cpp
@@ -1,32 +1,45 @@
 //
 //  main.cpp
-//  squareRoot
+//  fibonacci
 //
 //  Created by Gregory Francois on 4/18/19.
 //  Copyright © 2019 Gregory Francois. All rights reserved.
 //
 
 #include <iostream>
-#include <cmath>
-
-double squareRoot(double& r)
-{
-    double sqRt;
-    sqRt = sqrt(r);
-    return sqRt;
-};
 
 int main()
 {
-    // Given a real number n, find the square root of n. For example, given n = 9, return 3.
+    // The Fibonacci sequence is a series where the next term is the sum of pervious two terms. The first two terms of the Fibonacci sequence is 0 followed by 1.
+    // The Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21
     using std::cout;
     using std::cin;
+    // declare variables to hold series number and first two and next terms
+    int n, firstNum = 0, secondNum = 1, nthNum = 0;
+    cout << "Enter the number of terms for the Fibonacci sequence: ";
+    cin >> n;
+    // printing out the terms
+    cout << "The first " << n << " numbers of the Fibonacci Series is: ";
+    for (int i = 0; i < n; i++)
+    {
+        // will always print the first trwo terms as 0 and 1
+        if (i <= 1)
+            nthNum = i;
+        else
+        {
+            // after printing out 0 and 1, next iteration will make i = 2
+            // thus the next number printed will be sum of previous two
+            nthNum = firstNum + secondNum;
+            // moves secondNum to firstNum
+            firstNum = secondNum;
+            // moves nthNum to secondNum in sequence
+            secondNum = nthNum;
+        }
+        cout << nthNum << " ";
+    }
     
-    double num;
-    cout << "Enter a number: \n";
-    cin >> num;
+    cout << "\n";
     
-    cout << squareRoot(num) << "\n";
     
     return 0;
 }
