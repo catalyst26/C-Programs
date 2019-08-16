3adcdd2025a6a3ace082dc3cd344596994800c7c
catalyst26 <43795576+catalyst26@users.noreply.github.com>
2019-08-15 18:26:11 -0700

    Add files via upload
    Euclid's Algorithm for finding the GCD between two numbers

--- main.cpp
+++ main.cpp
@@ -1,45 +1,32 @@
 //
 //  main.cpp
-//  fibonacci
+//  euclidAlgo
 //
-//  Created by Gregory Francois on 4/18/19.
+//  Created by Gregory Francois on 8/8/19.
 //  Copyright © 2019 Gregory Francois. All rights reserved.
 //
 
 #include <iostream>
 
-int main()
+int euclidAlgo(int m, int n)
 {
-    // The Fibonacci sequence is a series where the next term is the sum of pervious two terms. The first two terms of the Fibonacci sequence is 0 followed by 1.
-    // The Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21
-    using std::cout;
-    using std::cin;
-    // declare variables to hold series number and first two and next terms
-    int n, firstNum = 0, secondNum = 1, nthNum = 0;
-    cout << "Enter the number of terms for the Fibonacci sequence: ";
-    cin >> n;
-    // printing out the terms
-    cout << "The first " << n << " numbers of the Fibonacci Series is: ";
-    for (int i = 0; i < n; i++)
+    int r;  // remainder variable
+    while (n != 0)
     {
-        // will always print the first trwo terms as 0 and 1
-        if (i <= 1)
-            nthNum = i;
-        else
-        {
-            // after printing out 0 and 1, next iteration will make i = 2
-            // thus the next number printed will be sum of previous two
-            nthNum = firstNum + secondNum;
-            // moves secondNum to firstNum
-            firstNum = secondNum;
-            // moves nthNum to secondNum in sequence
-            secondNum = nthNum;
-        }
-        cout << nthNum << " ";
+        r = m % n;  // assign remainder of division to r variable
+        m = n;      // n becomes new dividend
+        n = r;      // assign remainder to n
     }
+    return m;
+};
+
+int main() {
+    using std::cout;
     
-    cout << "\n";
+    int one = 31415;
+    int two = 14142;
     
+    cout << "GCD of " << one << " and " << two << " is " << euclidAlgo(one,two) << "\n";
     
     return 0;
 }
